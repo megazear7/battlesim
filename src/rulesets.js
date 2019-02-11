789dbf3a04b20a623d583c1d57e67855dd6efb65
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-11 07:20:14 -0500

    Added some basic civil war rules.

--- src/rulesets.js
+++ src/rulesets.js
@@ -1,32 +1,32 @@
 export default [
   {
-    name: "Civil War",
+    name: "Civil War Rules",
     sections: [
       {
         heading: 'Setup',
-        text: 'Some example text for this section heading.',
+        text: 'Setup will be based upon the scenario that you are playing',
         subsections: [
           {
             heading: 'Terrain',
-            text: 'Some example text for this section heading.'
+            text: 'Follow the terrain of the scenario or randomly setup terrain.'
           },
           {
             heading: 'Unit Placement',
-            text: 'Some example text for this section heading.'
+            text: 'Follow the unit placement of the scenario or take turns setting up units.'
           }
         ]
       },
       {
         heading: 'Taking Actions',
-        text: 'Some example text for this section heading.',
+        text: 'Use the fight page to take actions. Do not move any units until after submitting the action in the app. It will tell you what the result of the action is and what you are allowed to do.',
         subsections: [
           {
             heading: 'Selecting an action',
-            text: 'Some example text for this section heading.'
+            text: 'Any action can be selected but based upon the morale and energy of the unit they may be incapable of completing the action or they might refuse.'
           },
           {
             heading: 'Applying the result',
-            text: 'Some example text for this section heading.'
+            text: 'Apply the result exactly as it is written. Once you submit your action that is your orders to the unit. What the unit actually does is provided in the action result.'
           }
         ]
       }
