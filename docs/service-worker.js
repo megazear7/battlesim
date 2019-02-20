73ab7d30c59b4656cd4df20d04e1993f2bd3bc73
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-20 05:56:01 -0500

    Updated build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","84b2fc53728bfe42237cd8b3134e28e2"],["manifest.json","b920acb4df50b3dbc95acc75084bc665"],["src/components/battle-sim.js","b5a815f9b5f17c1e9a48b142e6c72694"],["src/components/battle-view.js","76af925aec33a8e7ce7abee30661246b"],["src/components/fight-view.js","0a8dd02de7301cc39aa6a2a3d42e198c"],["src/components/rules-view.js","9292f60f7e3b86ee4e7c4bfa51f8f75b"],["src/components/test-view.js","4c1224065874c90718b96109b3063c81"],["src/components/view-404.js","e2879b361013e19ce20a91f474d5a04e"],["src/components/war-view.js","2a61a078a3171b56b4e0477fea528366"]];
+var precacheConfig = [["index.html","84b2fc53728bfe42237cd8b3134e28e2"],["manifest.json","b920acb4df50b3dbc95acc75084bc665"],["src/components/battle-sim.js","6b4b2f0148033ed4c5ac83b6f309c8b6"],["src/components/battle-view.js","76af925aec33a8e7ce7abee30661246b"],["src/components/fight-view.js","23c8ede26cf8781374c48dda58b6d206"],["src/components/rules-view.js","9292f60f7e3b86ee4e7c4bfa51f8f75b"],["src/components/test-view.js","4c1224065874c90718b96109b3063c81"],["src/components/view-404.js","e2879b361013e19ce20a91f474d5a04e"],["src/components/war-view.js","2a61a078a3171b56b4e0477fea528366"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
