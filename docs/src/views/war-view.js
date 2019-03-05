aff9a6bdce4593e693b67a96eb5496e0f3a7e1a6
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-03-04 21:10:11 -0500

    Updated build

--- docs/src/views/war-view.js
+++ docs/src/views/war-view.js
@@ -103,7 +103,7 @@ class WarView extends connect(store)(PageViewElement) {
             <button @click="${this._create}">Create</button>
           </button-tray>
           <battle-sim-alert warning>You must select a ruleset to play and a battle to fight.</battle-sim-alert>
-          <input id="name" type="text" placeholder="Battle name"></input>
+          <input id="battle-name" type="text" placeholder="Battle name"></input>
           <input id="army1-name" type="text" placeholder="First army name"></input>
           <input id="army2-name" type="text" placeholder="Second army name"></input>
         </div>
@@ -240,7 +240,7 @@ class WarView extends connect(store)(PageViewElement) {
   }
 
   get newBattleNameElement() {
-    return this.shadowRoot.getElementById('name');
+    return this.shadowRoot.getElementById('battle-name');
   }
 
   get newBattleArmy1NameElement() {
