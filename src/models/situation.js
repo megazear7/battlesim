7b76258868b5e6ebba97495604f878aa5b81730a
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-03-08 08:03:51 -0500

    Fixed issue with the secondsSpentMoving calculation.

--- src/models/situation.js
+++ src/models/situation.js
@@ -55,7 +55,7 @@ export default class Situation {
       // This implys that the users wants to move as far as possible.
       return this.secondsAvailableToMove;
     } else if (this.soloUnit.speed > 0) {
-      return Math.min(this.distance / this.soloUnit.speed, this.secondsAvailableToMove);
+      return Math.min(this.distanceInYards / this.soloUnit.speed, this.secondsAvailableToMove);
     } else {
       return 0;
     }
