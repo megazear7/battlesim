888e525c99c0a35186a19f06e680a8ab6f9faab2
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-03-12 05:51:50 -0400

    Updated build

--- docs/src/views/fight-view.js
+++ docs/src/views/fight-view.js
@@ -1259,6 +1259,14 @@ class FightView extends BattleViewWrapper {
     }
   }
 
+  get separation() {
+    if (this.get('separation')) {
+      return parseInt(this.get('separation').value);
+    } else {
+      return null;
+    }
+  }
+
   get target() {
     if (this.get('target')) {
       return parseInt(this.get('target').value);
